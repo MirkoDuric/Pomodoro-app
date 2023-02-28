22ae6adde09c4ee8e2f98f19f71e4b1a27322c85
MirkoDuric <mirkodjuricczv@gmail.com>
2023-02-28 14:10:47 +0100

    Change to brake counter when learning counter reaches 00:00 done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,34 @@
 import { useState, useEffect } from "react";
 import Button from "./Button";
 export default function Counter() {
-  const [learningCounterMin, setLearningCounterMin] = useState(1);
-  const [learningCounterSec, setLearningCounterSec] = useState(1);
+  const [learningCounterMin, setLearningCounterMin] = useState(0);
+  const [learningCounterSec, setLearningCounterSec] = useState(10);
   const [isCounterRunning, setIsCounterRunning] = useState(false);
+  const [brakeCounterRunning, setBrakeCounterRunning] = useState(false);
+  const [brakeCounterSec, setBrakeCounterSec] = useState(59);
+  const [brakeCounterMin, setBrakeCounterMin] = useState(4);
+  useEffect(() => {
+    let intervalId;
 
+    if (brakeCounterRunning) {
+      intervalId = setInterval(() => {
+        if (brakeCounterSec === 0) {
+          setBrakeCounterMin((prevMin) => prevMin - 1);
+          setBrakeCounterSec(59);
+        } else {
+          setLearningCounterSec((prevSec) => prevSec - 1);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [brakeCounterRunning, brakeCounterSec]);
+  useEffect(() => {
+    if (brakeCounterMin === 0 && brakeCounterSec === 0) {
+      setBrakeCounterRunning(false);
+      setBrakeCounterMin(4);
+      setBrakeCounterSec(59);
+    }
+  }, [brakeCounterRunning, brakeCounterSec]);
   useEffect(() => {
     let intervalId;
 
@@ -21,7 +45,14 @@ export default function Counter() {
 
     return () => clearInterval(intervalId);
   }, [isCounterRunning, learningCounterSec]);
-
+  useEffect(() => {
+    if (learningCounterMin === 0 && learningCounterSec === 0) {
+      setIsCounterRunning(false);
+      setLearningCounterMin(29);
+      setLearningCounterSec(59);
+      setBrakeCounterRunning(true);
+    }
+  }, [isCounterRunning, learningCounterSec]);
   function handleStartButtonClick() {
     setIsCounterRunning(true);
   }
@@ -34,14 +65,20 @@ export default function Counter() {
     setLearningCounterMin(29);
     setLearningCounterSec(59);
   }
-  function restartClockAndStop() {
-    setIsCounterRunning(false);
-    setLearningCounterMin(29);
-    setLearningCounterSec(59);
-  }
+
   return (
     <div>
-      {learningCounterMin.toString()}:{learningCounterSec.toString()}
+      <div>
+        {brakeCounterRunning ? (
+          <div>
+            {brakeCounterMin.toString()}:{brakeCounterSec.toString()}
+          </div>
+        ) : (
+          <div>
+            {learningCounterMin.toString()}:{learningCounterSec.toString()}
+          </div>
+        )}
+      </div>
       <div>
         <Button name={"Start"} onClick={handleStartButtonClick}></Button>
         <Button name={"Stop"} onClick={handleStopButtonClick}></Button>
